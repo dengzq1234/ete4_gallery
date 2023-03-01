b2de070fa46480de0be967187237e827b6b5051e
Jordi <jordi.burguet.castell@gmail.com>
2023-03-01 16:19:15 +0100

    Small simplifications.

--- ete4_imgface.py
+++ ete4_imgface.py
@@ -1,33 +1,37 @@
-from ete4 import Tree
-# from staple_layouts import LayoutBarplot
-from ete4.smartview import TreeStyle, NodeStyle, TreeLayout
-from ete4.smartview  import RectFace, ImgFace
+#!/usr/bin/env python3
 
 import os
 
-TREEFILE = './basic_example1_annotated.nw'
-ABSOLUTE_IMGPATH = os.path.abspath('./frog.jpeg')
+from ete4 import Tree
+from ete4.smartview import TreeLayout, ImgFace
+
+
+TREEFILE = 'basic_example1_annotated.nw'
+ABSOLUTE_IMGPATH = os.path.abspath('frog.jpeg')
+
 popup_prop_keys = [
-                'name', 'dist', 'support', 'sample1',
-                'sample2','sample3','sample4','sample5',
-                'random_type','bool_type','bool_type2'
-        ]
+    'name', 'dist', 'support', 'sample1',
+    'sample2', 'sample3', 'sample4', 'sample5',
+    'random_type', 'bool_type', 'bool_type2'
+]
 
 t = Tree(TREEFILE, format=1)
-level = 2 #level 1 is leaf name
+level = 2  # level 1 is leaf name
 
-def get_face(level, prop):
+
+def get_face(prop):
     def layout_fn(node):
         if node.is_leaf():
-            face = ImgFace(img_path=ABSOLUTE_IMGPATH, width=70, height=50, name="", 
-            padding_x=0, padding_y=0)
-            node.add_face(face, position="aligned", column=level)
+            face = ImgFace(
+                img_path=ABSOLUTE_IMGPATH, width=70, height=50, name='',
+                padding_x=0, padding_y=0)
+            node.add_face(face, position='aligned', column=level)
+
     return layout_fn
 
 
 layouts = [
-    TreeLayout(name='sample1', ns=get_face(level, 'sample1'), aligned_faces = True),
+    TreeLayout(name='sample1', ns=get_face('sample1'), aligned_faces=True),
 ]
 
-t.explore(tree_name='example',layouts=layouts, \
-            popup_prop_keys=popup_prop_keys, port=5000)
+t.explore(tree_name='example', layouts=layouts, popup_prop_keys=popup_prop_keys)
--- ete4_rectface.py
+++ ete4_rectface.py
@@ -1,38 +1,38 @@
+#!/usr/bin/env python3
+
 from ete4 import Tree
-# from staple_layouts import LayoutBarplot
-from ete4.smartview import TreeStyle, NodeStyle, TreeLayout
-from ete4.smartview  import (RectFace, CircleFace, SeqMotifFace, AttrFace,
-                        TextFace, OutlineFace, LegendFace)
+from ete4.smartview import TreeLayout, RectFace
 
 
-
-TREEFILE = './basic_example1_annotated.nw'
+TREEFILE = 'basic_example1_annotated.nw'
 
 popup_prop_keys = [
-                'name', 'dist', 'support', 'sample1',
-                'sample2','sample3','sample4','sample5',
-                'random_type','bool_type','bool_type2'
-        ]
+    'name', 'dist', 'support', 'sample1',
+    'sample2', 'sample3', 'sample4', 'sample5',
+    'random_type', 'bool_type', 'bool_type2'
+]
+
 
 t = Tree(TREEFILE, format=1)
-level = 2 #level 1 is leaf name
+level = 2  # level 1 is leaf name
 
-def get_face(level):
-    def layout_fn(node):
-        if node.is_leaf():
-            rect_face = RectFace(width=50, height=70, color="gray", 
-            opacity=0.7, text=None, fgcolor='black', # text color
-            min_fsize=6, max_fsize=15, ftype='sans-serif',
-            padding_x=0, padding_y=0,
-            tooltip=None)
-            node.add_face(rect_face, position="aligned", column=level)
-    return layout_fn
+
+def layout_rect(node):
+    if not node.is_leaf():
+        return
+
+    rect_face = RectFace(
+        width=50, height=70, color='gray',
+        opacity=0.7, text=None, fgcolor='black',
+        min_fsize=6, max_fsize=15, ftype='sans-serif',
+        padding_x=0, padding_y=0,
+        tooltip=None)
+
+    node.add_face(rect_face, position='aligned', column=level)
 
 
 layouts = [
-    # Rectangular face
-    TreeLayout(name='sample1', ns=get_face(level),aligned_faces = True),
+    TreeLayout(name='sample1', ns=layout_rect, aligned_faces=True),
 ]
 
-t.explore(tree_name='example',layouts=layouts, \
-            popup_prop_keys=popup_prop_keys, port=5000)
+t.explore(tree_name='example', layouts=layouts, popup_prop_keys=popup_prop_keys)
