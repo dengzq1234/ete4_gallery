3b44a1d2f3d73d6996a04424c3a69b2572109176
Jordi <jordi.burguet.castell@gmail.com>
2023-03-01 16:07:44 +0100

    ete4_arrowface.py: simplify

--- ete4_arrowface.py
+++ ete4_arrowface.py
@@ -1,42 +1,40 @@
+#!/usr/bin/env python3
+
 from ete4 import Tree
-# from staple_layouts import LayoutBarplot
-from ete4.smartview import TreeStyle, NodeStyle, TreeLayout
-from ete4.smartview  import (RectFace, CircleFace, SeqMotifFace, AttrFace,
-                        ArrowFace, TextFace, OutlineFace, LegendFace)
+from ete4.smartview import TreeLayout, ArrowFace
 
 
-
-TREEFILE = './basic_example1_annotated.nw'
+TREEFILE = 'basic_example1_annotated.nw'
 
 popup_prop_keys = [
-                'name', 'dist', 'support', 'sample1',
-                'sample2','sample3','sample4','sample5',
-                'random_type','bool_type','bool_type2'
-        ]
+    'name', 'dist', 'support', 'sample1',
+    'sample2','sample3','sample4','sample5',
+    'random_type','bool_type','bool_type2'
+]
 
 t = Tree(TREEFILE, format=1)
-level = 2 #level 1 is leaf name
+level = 2  # level 1 is leaf name
 
-def get_face(level):
-    def layout_fn(node):
-        if node.is_leaf():
-            face = ArrowFace(width=100, height=70, orientation='right',
-            color='green', 
-            stroke_color='gray', stroke_width='1.5px',
-            text=None, fgcolor='black', # text color
-            min_fsize=6, max_fsize=15,
-            ftype='sans-serif',
-            name="",
-            padding_x=0, padding_y=0,
-            tooltip=None)
-            node.add_face(face, position="aligned", column=level)
-    return layout_fn
+
+def layout_arrow(node):
+    if not node.is_leaf():
+        return
+
+    face = ArrowFace(
+        width=100, height=70, orientation='right',
+        color='green', stroke_color='gray', stroke_width='1.5px',
+        text=None, fgcolor='black',
+        min_fsize=6, max_fsize=15,
+        ftype='sans-serif',
+        name='',
+        padding_x=0, padding_y=0,
+        tooltip=None)
+
+    node.add_face(face, position='aligned', column=level)
 
 
 layouts = [
-    # Rectangular face
-    TreeLayout(name='sample1', ns=get_face(level),aligned_faces = True),
+    TreeLayout(name='sample1', ns=layout_arrow, aligned_faces=True),
 ]
 
-t.explore(tree_name='example',layouts=layouts, \
-            popup_prop_keys=popup_prop_keys, port=5000)
+t.explore(tree_name='example', layouts=layouts, popup_prop_keys=popup_prop_keys)
