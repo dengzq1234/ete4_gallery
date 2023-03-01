6efd4d58b1e821de04c3f7873027bd9884303972
Jordi <jordi.burguet.castell@gmail.com>
2023-03-01 15:54:05 +0100

    cosmetics

--- ete4_alignlinkface.py
+++ ete4_alignlinkface.py
@@ -3,7 +3,6 @@
 from ete4 import Tree
 from ete4.smartview import TreeLayout, AlignLinkFace
 
-
 TREEFILE = 'basic_example1_annotated.nw'
 
 popup_prop_keys = [
@@ -20,24 +19,17 @@ def layout_align_link(node):
     align_link_face = AlignLinkFace(
         stroke_color='blue', stroke_width=0.5, line_type=1, opacity=0.8)
 
-    if node.is_leaf():
-        node.add_face(align_link_face,
-                      position='branch_right',
-                      column=1e9)
-    else:
-        node.add_face(align_link_face,
-                      position='branch_right',
-                      column=1e9,
-                      collapsed_only=True)
+    node.add_face(align_link_face,
+                  position='branch_right',
+                  column=1e9,
+                  collapsed_only=not node.is_leaf())
 
 layout_align_link.__name__ = 'Aligned panel link'
 layout_align_link._module = 'default'
 
 
-
 layouts = [
     TreeLayout(name='sample1', ns=layout_align_link, aligned_faces=True),
 ]
-
 
 t.explore(tree_name='example',layouts=layouts, popup_prop_keys=popup_prop_keys)
