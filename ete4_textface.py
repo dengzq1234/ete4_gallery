9159dca9ec1e89476aad94669cf119c49806671e
Jordi <jordi.burguet.castell@gmail.com>
2023-03-13 17:55:12 +0100

    ete4_textface.py: simplify and fix error.
    The error came from passing the argument height=None to
    TextFace, but TextFace does not accept that argument anymore
    (it used to, but it was not using it at all, so it was removed).

--- ete4_textface.py
+++ ete4_textface.py
@@ -1,35 +1,34 @@
-from ete4 import Tree
-# from staple_layouts import LayoutBarplot
-from ete4.smartview import TreeStyle, NodeStyle, TreeLayout
-from ete4.smartview  import (RectFace, CircleFace, SeqMotifFace, AttrFace,
-                        TextFace, OutlineFace, LegendFace)
+#!/usr/bin/env python3
 
+from ete4 import Tree
+from ete4.smartview import TreeLayout, TextFace
 
 
 TREEFILE = 'example_data/tree.nw'
 
 popup_prop_keys = [
-                'name', 'dist', 'support', 'sample1',
-                'sample2','sample3','sample4','sample5',
-                'random_type','bool_type','bool_type2'
-        ]
-
-t = Tree(TREEFILE, format=1)
-level = 2 #level 1 is leaf name
-
-
-def get_face(level, prop):
-    def layout_fn(node):
-        if node.is_leaf():
-            text_face = TextFace(node.props.get(prop), color="black",
-            min_fsize=6, max_fsize=15, ftype='sans-serif',
-            padding_x=0, padding_y=0, width=None, height=None)
-            node.add_face(text_face, position="aligned", column=level)
-    return layout_fn
-
-layouts = [
-    TreeLayout(name='sample1', ns=get_face(level, 'sample1'),aligned_faces = True),
+    'name', 'dist', 'support', 'sample1',
+    'sample2', 'sample3', 'sample4', 'sample5',
+    'random_type', 'bool_type', 'bool_type2'
 ]
 
-t.explore(tree_name='example',layouts=layouts, \
-            popup_prop_keys=popup_prop_keys, port=5000)
+t = Tree(TREEFILE, format=1)
+level = 2  # level 1 is leaf name
+
+def get_face(prop):
+    def layout_fn(node):
+        if node.is_leaf():
+            text_face = TextFace(
+                node.props.get(prop), color='black',
+                min_fsize=6, max_fsize=15, ftype='sans-serif',
+                padding_x=0, padding_y=0, width=None)
+            node.add_face(text_face, position='aligned', column=level)
+
+    return layout_fn
+
+
+layouts = [
+    TreeLayout(name='sample1', ns=get_face('sample1'), aligned_faces=True),
+]
+
+t.explore(tree_name='example', layouts=layouts, popup_prop_keys=popup_prop_keys)
--- readme.md
+++ readme.md
@@ -5,7 +5,6 @@ A demostration of some of the capabilities of ETE v.4.
 
 ## Issues
 
-- `ete4_textface.py` - Internal server error
 - `ete4_attrface.py` - Seems to not add any face.
 - `ete4_htmlface.py` - Seems to not add any face.
 - `ete4_outline.py` - Once the outline appears, it never disappears.
