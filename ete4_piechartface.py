7914a0c5fa802280ee5c678cdd033a241c7295e9
Jordi <jordi.burguet.castell@gmail.com>
2023-03-13 18:01:24 +0100

    ete4_piechartface.py: simplify and remove comment on internal server error.
    With the update of ete (for example in commit 31cf5b8), the piechartface does not
    produce any error anymore. It was related to setting the color of the parent
    CircleFace.

--- ete4_piechartface.py
+++ ete4_piechartface.py
@@ -1,38 +1,37 @@
-from ete4 import Tree
-# from staple_layouts import LayoutBarplot
-from ete4.smartview import TreeStyle, NodeStyle, TreeLayout
-from ete4.smartview  import CircleFace, PieChartFace
+#!/usr/bin/env python3
 
+from ete4 import Tree
+from ete4.smartview import TreeLayout, PieChartFace
 
 
 TREEFILE = 'example_data/tree.nw'
 
 popup_prop_keys = [
-                'name', 'dist', 'support', 'sample1',
-                'sample2','sample3','sample4','sample5',
-                'random_type','bool_type','bool_type2'
-        ]
+    'name', 'dist', 'support', 'sample1',
+    'sample2', 'sample3', 'sample4', 'sample5',
+    'random_type', 'bool_type', 'bool_type2'
+]
 
 t = Tree(TREEFILE, format=1)
-level = 2 #level 1 is leaf name
+level = 2  # level 1 is leaf name
 
-def get_face(level, prop):
+def get_face(prop):
     def layout_fn(node):
         if node.is_leaf():
             piechart_data = [
-                ["pie1", 4, "red", None],
-                ["pie2", 10, "blue", None],
-                ["pie3", 20, "green", None],
-                ]
+                ['pie1', 4, 'red', None],
+                ['pie2', 10, 'blue', None],
+                ['pie3', 20, 'green', None],
+            ]
             face = PieChartFace(radius=20, data=piechart_data, name=prop,
-            padding_x=2,padding_y=2,tooltip=None)
-            node.add_face(face, position="aligned", column=level)
+                                padding_x=2, padding_y=2, tooltip=None)
+            node.add_face(face, position='aligned', column=level)
+
     return layout_fn
 
 
 layouts = [
-    TreeLayout(name='sample1', ns=get_face(level, 'sample1'), aligned_faces = True),
+    TreeLayout(name='sample1', ns=get_face('sample1'), aligned_faces=True),
 ]
 
-t.explore(tree_name='example',layouts=layouts, \
-            popup_prop_keys=popup_prop_keys, port=5000)
+t.explore(tree_name='example', layouts=layouts, popup_prop_keys=popup_prop_keys)
--- readme.md
+++ readme.md
@@ -8,4 +8,3 @@ A demostration of some of the capabilities of ETE v.4.
 - `ete4_attrface.py` - Seems to not add any face.
 - `ete4_htmlface.py` - Seems to not add any face.
 - `ete4_outline.py` - Once the outline appears, it never disappears.
-- `ete4_piechartface.py` - Internal server error.
