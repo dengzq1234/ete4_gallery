847e329c59f089a60981c6484142d3e7525b4d8c
dengzq1234 <dengziqi1234@gmail.com>
2023-11-05 22:12:01 +0100

    update

--- ete4_seqface.py
+++ ete4_seqface.py
@@ -54,67 +54,90 @@ def get_pfams(pfamoutput):
     return pfams
 
 
+# get information alignment and domains
 name2pfams = get_pfams(PROTEIN2DOMAIN)
 name2seq = get_seqs(MSA)
+
+
 for leaf in t:
     leaf.add_prop('seq', name2seq[leaf.name])
 
 
-def layout_alnface(node):
-    if not node.is_leaf:
-        return
+def layout_alnface_gray(node):
+    if node.is_leaf:
+        seq_face = AlignmentFace(
+            node.props.get('seq'),
+            seqtype='aa', gap_format='line', seq_format='[]',
+            width=800, height=None,
+            fgcolor='black', bgcolor='#bcc3d0', gapcolor='gray',
+            gap_linewidth=0.2,
+            max_fsize=12, ftype='sans-serif',
+            padding_x=0, padding_y=0)
 
-    seq_face = AlignmentFace(
-        node.props.get('seq'),
-        seqtype='aa', gap_format='line', seq_format='[]',
-        width=None, height=None,
-        fgcolor='black', bgcolor='#bcc3d0', gapcolor='gray',
-        gap_linewidth=0.2,
-        max_fsize=12, ftype='sans-serif',
-        padding_x=0, padding_y=0)
+        node.add_face(seq_face, position='aligned')
+    return
 
-    node.add_face(seq_face, position='aligned')
+def layout_alnface_compact(node):
+    if node.is_leaf:
+        seq_face = AlignmentFace(
+            node.props.get('seq'),
+            seqtype='aa', gap_format='line', seq_format='compactseq',
+            width=800, height=None,
+            fgcolor='black', bgcolor='#bcc3d0', gapcolor='gray',
+            gap_linewidth=0.2,
+            max_fsize=12, ftype='sans-serif',
+            padding_x=0, padding_y=0)
 
+        node.add_face(seq_face, position='aligned')
+    return
 
 def layout_seqface(node):
-    if not node.is_leaf:
-        return
+    if node.is_leaf:
+       
+        seq_face = SeqFace(
+            node.props.get('seq'),
+            seqtype='aa', poswidth=1, 
+            draw_text=True, max_fsize=15, ftype='sans-serif',
+            padding_x=0, padding_y=0)
 
-    seq_face = SeqFace(
-        node.props.get('seq'),
-        seqtype='aa', poswidth=15, 
-        draw_text=True, max_fsize=15, ftype='sans-serif',
-        padding_x=0, padding_y=0)
-
-    node.add_face(seq_face, position='aligned')
+        node.add_face(seq_face, position='aligned')
+    return
 
 
-def layout_seqmotifface(node):
-    if not node.is_leaf or node.name not in name2pfams:
-        return
+# def layout_seqmotifface(node):
+#     if not node.is_leaf or node.name not in name2pfams:
+#         return
 
-    prot_domains = name2pfams[node.name]
-    doms = [[start, end, '()',
-             None, None, 'gray', 'gray',
-             f'arial|20|black|{name}'] for name, start, end in prot_domains]
+#     prot_domains = name2pfams[node.name]
+    
+#     # doms = [
+#     #     [start, end, shape, posw, h, fg, bg, text, opacity]
+#     # ]
+#     # shape: (), [], line, -, <>, o, seq, compactseq, ^, >, <, v
 
-    seqFace = SeqMotifFace(
-        name2seq[node.name],
-        motifs=doms, seqtype='aa',
-        gap_format='line', seq_format='[]',
-        width=None, height=None,
-        fgcolor='black', bgcolor='#bcc3d0', gapcolor='gray',
-        gap_linewidth=0.2,
-        max_fsize=12, ftype='sans-serif',
-        padding_x=0, padding_y=0)
+#     domain_shapre = '()'
+#     doms = [[start, end, domain_shapre,
+#              None, None, 'brown', 'brown',
+#              f'arial|20|black|{name}'] for name, start, end in prot_domains]
 
-    node.add_face(seqFace, position='aligned', column=0)
+#     seqFace = SeqMotifFace(
+#         name2seq[node.name],
+#         motifs=doms, seqtype='aa',
+#         gap_format='line', seq_format='[]',
+#         width=None, height=None,
+#         fgcolor='black', bgcolor='#bcc3d0', gapcolor='gray',
+#         gap_linewidth=0.2,
+#         max_fsize=12, ftype='sans-serif',
+#         padding_x=0, padding_y=0)
+
+#     node.add_face(seqFace, position='aligned', column=0)
 
 
 layouts = [
-    TreeLayout(name='aln', ns=layout_alnface, aligned_faces=True),
-    TreeLayout(name='seq', ns=layout_seqface, aligned_faces=True),
-    TreeLayout(name='pfam', ns=layout_seqmotifface, aligned_faces=True),
+    TreeLayout(name='compact_aln', ns=layout_alnface_compact, aligned_faces=True),
+    TreeLayout(name='gray_aln', ns=layout_alnface_gray, aligned_faces=True, active=False),
+    TreeLayout(name='seq', ns=layout_seqface, aligned_faces=True,  active=False),
+    #TreeLayout(name='domain', ns=layout_seqmotifface, aligned_faces=True),
 ]
 
 t.explore(layouts=layouts, keep_server=True)
