36c7e8fb6615bd220605a969baf994b57a0336de
dengzq1234 <dengziqi1234@gmail.com>
2023-11-05 13:21:47 +0100

    update

--- smartview/node_backgrounds.py
+++ smartview/node_backgrounds.py
@@ -19,12 +19,26 @@ n2 = t.common_ancestor(["b1", "b2", "b3", "b4"])
 n3 = t.common_ancestor(["c1", "c2", "c3"])
 n4 = t.common_ancestor(["b3", "b4"])
 
+# set color map dictionary
+colormap = {
+    "ancestor_a": "LightSteelBlue",
+    "ancestor_b": "Moccasin",
+    "ancestor_c": "DarkSeaGreen",
+    "ancestor_d": "Khaki"
+}
+
+def get_tree_style(colormap):
+    def add_legend(tree_style):
+        tree_style.add_legend(
+            title = "MyLegend", 
+            variable = "discrete", 
+            colormap = colormap
+            )
+    return add_legend
 
 def get_background(node):
-
     # make node name with bigger text
     node.add_face(TextFace(node.name, min_fsize=6, max_fsize=25), column=0, position="branch_right")
-
     # set node style
     if node == n1:
         node.set_style(nst1)
@@ -37,7 +51,12 @@ def get_background(node):
     return 
 
 # Create a TreeLayout object, passing in the function
-tree_layout = TreeLayout(name="MyTreeLayout", ns=get_background)
+tree_layout = TreeLayout(
+    name="MyTreeLayout", 
+    ns=get_background, 
+    ts=get_tree_style(colormap),
+    active=True)
+
 layouts = []
 layouts.append(tree_layout)
 t.explore(keep_server=True, layouts=layouts)
