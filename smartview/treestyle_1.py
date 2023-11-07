a9009ed13cac881b4339c0475da1173aa4762d01
Jordi <jordi.burguet.castell@gmail.com>
2023-11-07 15:09:05 +0100

    cosmetics

--- smartview/treestyle_1.py
+++ smartview/treestyle_1.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 from ete4 import Tree
 from ete4.smartview import TreeStyle, TreeLayout
 
@@ -7,11 +9,12 @@ t.populate(20, random_branches=True)
 def modify_tree_style(tree_style):
     #tree_style.aligned_grid = False
     tree_style.collapse_size = 70
-    
+
 
 # Create a TreeLayout object, passing in the function
 tree_layout = TreeLayout(name="MyTreeLayout", ts=modify_tree_style)
 
-layouts = []
-layouts.append(tree_layout)
-t.explore(keep_server=True, layouts=layouts)
+layouts = [tree_layout]
+
+t.explore(layouts=layouts)
+input('Tree explorer running. Press enter to stop the server and finish.\n')
