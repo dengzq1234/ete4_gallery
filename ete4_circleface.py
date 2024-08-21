a88bae534bfecfd261e181484fae660213e1dcef
Jordi <jordi.burguet.castell@gmail.com>
2024-08-21 14:45:08 +0200

    Update to new layouts in ete, in branch faster.

--- ete4_circleface.py
+++ ete4_circleface.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 from random import random, randint, choice
-from functools import cache
 
 from ete4 import Tree
 from ete4.smartview import Layout, DEFAULT_LAYOUT, Decoration, CircleFace
@@ -11,14 +10,13 @@ t.populate(20, dist_fn=random, support_fn=random)
 
 colors = ['red', 'blue', 'green']  # color to choose from
 
-@cache
-def node_decorations(node):
+def draw_node(node):
     if node.is_leaf:
         face = CircleFace(rmax=randint(2, 20),
                           style={'fill': choice(colors)})
         return Decoration(face, position='aligned')
 
-circles_layout = Layout(name='circles', node_decos=node_decorations)
+circles_layout = Layout(name='circles', draw_node=draw_node)
 
 t.explore(layouts=[DEFAULT_LAYOUT, circles_layout])
 input('Tree explorer running. Press enter to stop the server and finish.')
--- ete4_rectface.py
+++ ete4_rectface.py
@@ -8,13 +8,13 @@ from ete4.smartview import Layout, DEFAULT_LAYOUT, Decoration, RectFace
 t = Tree()
 t.populate(20, dist_fn=random, support_fn=random)
 
-def node_decorations(node):
+def draw_node(node):
     if node.is_leaf:
         face = RectFace(wmax=80, hmax=70,
                         style={'fill': 'blue'})
         yield Decoration(face, position='aligned')
 
-rects_layout = Layout(name='rects', node_decos=node_decorations)
+rects_layout = Layout(name='rects', draw_node=draw_node)
 
 t.explore(layouts=[DEFAULT_LAYOUT, rects_layout])
 input('Tree explorer running. Press enter to stop the server and finish.\n')
