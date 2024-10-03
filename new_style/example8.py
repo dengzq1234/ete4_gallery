3e1e7b53b1fd4b4565502a0a5a9ea55465cc5347
Jordi <jordi.burguet.castell@gmail.com>
2024-10-03 09:26:21 -0400

    Practice new dot.shape and dot.radius options.

--- new_style/example8.py
+++ new_style/example8.py
@@ -21,6 +21,7 @@ for node in t.traverse():
 tree_style = {
     'shape': 'circular',
     'min-size': 15,
+    'dot': {'shape': "square", 'radius': 10, 'fill': 'green'},
     'hz-line': {'stroke': 'red', 'stroke-width': 5},
     'vt-line': {'stroke': '#ffff00', 'stroke-width': 3},
     'box': {'fill': '#e0e0e0'},
@@ -42,7 +43,11 @@ def node_style(node, collapsed):
         yield {'box': {'border': '4px',
                        'stroke': 'blue',
                        'fill': 'green'},
-               'dot': {'fill': 'red'},
+               'dot': {'fill': 'red',
+                       'shape': 'hexagon',
+                       'stroke': 'yellow',
+                       'opacity': 1,
+                       'radius': 20},
                'hz-line': {'stroke-width': 2}}
         face = BoxFace(wmax=80, hmax=70,
                        style={'fill': 'lightblue'},
